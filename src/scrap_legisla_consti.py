f8fc2e4bd58dd5990b29007cc30b9e53cd42a014
lucaskeysm <lucas.mitsudo@revio.com.br>
2026-04-13 15:04:07 -0300

    feat: criação das constantes (status da modificação, tipo de modificação e tipo de referência), gerar id aleatório, criação e estruturação dos objetos de acordo com o modelo JSON fornecido, comentários adicionados.

--- src/scrap_legisla_consti.py
+++ src/scrap_legisla_consti.py
@@ -2,6 +2,7 @@ import time
 import json
 import uuid
 from pathlib import Path
+from typing import Any, Dict
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -18,36 +19,242 @@ URL = "https://www.planalto.gov.br/ccivil_03/Constituicao/Constituicao.htm"
 OUTPUT_DIR = Path("output_constituicao")
 OUTPUT_DIR.mkdir(exist_ok=True)
 
+# STATUS DA MODIFICAÇÃO
 STATUS_VIGENTE        = "VIGENTE"
 STATUS_REVOGADO       = "REVOGADO"
 STATUS_SUSPENSO       = "SUSPENSO"
 STATUS_REDACAO_ANTIGA = "REDACAO_ANTIGA"
 
+# TIPO DE MODIFICAÇÃO
 TIPO_INCLUSAO  = "INCLUSAO"
 TIPO_ALTERACAO = "ALTERACAO"
 TIPO_REVOGACAO = "REVOGACAO"
 TIPO_ADICAO    = "ADICAO"
 
+# TIPO DE REFERÊNCIA
 TIPO_REF_LEI       = "LEI"
 TIPO_REF_DECRETO   = "DECRETO"
 TIPO_REF_RESOLUCAO = "RESOLUCAO"
 TIPO_REF_OUTRO     = "OUTRO"
 
 
+# GERRAR ID
 def gerar_id():
     return str(uuid.uuid4())
 
+
+# MODIFICAÇÕES HISTÓRICAS
 def make_modificacoes_historicas (
-        tipo: str = TIPO_INCLUSAO,
-        descricao: str = "",
-        texto_antigo: str = "",
+    tipo: str = None,
+    descricao: str = "",
+    texto_antigo: str = "",
+    status: str = None,
+) -> Dict [str, Any]:
+    """
+    Schema:
+    {
+        "tipo_de_modificacao": "INCLUSAO | ALTERACAO | REVOGACAO | ADICAO",
+        "descricao_da_modificacao": "...",
+        "texto_antigo_descontinuado": "...",
+        "status_da_modificacao": "VIGENTE | REVOGADO | SUSPENSO | REDACAO_ANTIGA"
+    }
+    """
+    return {
+        "tipo_de_modificacao": tipo,
+        "descricao_da_modificacao": descricao,
+        "texto_antigo_ddescontinuado": texto_antigo,
+        "status_da_modificacao": status,
+    }
 
-):
+
+# REFERENCIAIS LEGISLATIVOS
+def make_referencial_legislativo(
+    numero: str = "",
+    tipo: str = None,
+    texto: str = "",
+    status: str = None,
+) -> Dict[str, Any]:
+    """
+    Schema:
+    {
+        "id_referencia": "...",
+        "numero_referencia": "...",
+        "tipo_de_referencia": "LEI | DECRETO | RESOLUCAO | OUTRO",
+        "texto_da_referencia": "...",
+        "status_da_modificacao": "VIGENTE | REVOGADO | SUSPENSO | REDACAO_ANTIGA"
+    }
+    """
+    return {
+        "id_referencia": gerar_id(),
+        "numero_referencia": numero,
+        "tipo_de_referencia": tipo,
+        "texto_da_referencia": texto,
+        "status_da_referencia": status,
+    }
+
+
+# TITULOS
+def make_titulo(
+    titulo: str = "",
+    descricao: str = "",
+) -> Dict[str, Any]:
+    """
+    Schema:
+    {
+        "id_dispositivo_legislativo": "...",
+        "titulo": "...",
+        "descricao_do_titulo": "...",
+        "capitulos": [...]
+    }
+    """
+    return{
+        "id_dispositivo_legislativo": gerar_id(),
+        "titulo": titulo,
+        "descricao_do_titulo": descricao,
+        "capitulos": [],
+    }
+
+
+# CAPÍTULO
+def make_capitulo(
+    capitulo: str = "",
+    descricao: str = "",
+) -> Dict[str, Any]:
+    """
+    Schema:
+    {
+        "id_dispositivo_legislativo": "...",
+        "capitulo": "...",
+        "descricao_titulo": "...",
+        "artigos": [...]
+    }
+    """
+    return{
+        "id_dispositivo_legislativo": gerar_id(),
+        "capitulo": capitulo,
+        "descricao_capitulo": descricao,
+        "artigos": [],
+    }
+
+# ARTIGOS
+def make_artigo(
+    numero: str = "",
+    caput: str = "",
+    status: str = None,
+) -> Dict[str, Any]:
+    """
+    Schema:
+    {
+        "id_dispositivo_legislativo": "...",
+        "numero_artigo": "...",
+        "caput": "...",
+        "status_do_artigo": "VIGENTE | REVOGADO | SUSPENSO | REDACAO_ANTIGA",
+        "referenciais_legislativos": [...],
+        "modificacoes_historicas": [...],
+        "paragrafos": [...]
+    }
+    """
+    return{
+        "id_dispositivo_leislativo": gerar_id(),
+        "numero_referencia": numero,
+        "caput": caput,
+        "status_do_artigo": status,
+        "referenciais_legislativos": [],
+        "modificacoes_historicas": [],
+        "paragrafos": [],
+    }
+
+
+# PARAGRAFOS
+def make_paragrafo(
+    numero: str = "",
+    texto: str = "",
+    status: str = None,
+) -> Dict[str, Any]:
+    """
+    Schema:
+    {
+        "id_dispositivo_legislativo": "...",
+        "numero_paragrafo": "...",
+        "texto_do_paragrafo": "...",
+        "status_do_paragrafo": "VIGENTE | REVOGADO | SUSPENSO | REDACAO_ANTIGA",
+        "refereiais_legislativos": [...],
+        "modificacoes_historicas": [...],
+        "incisos": [...]
+    }
+    """
+    return{
+        "id_dispositivo_legislativo": gerar_id(),
+        "numero_paragrafo": numero,
+        "texto_paragrafo": texto,
+        "status_paragrafo": status,
+        "referenciais_legislativos": [],
+        "modificacoes_historicas": [],
+        "incisos": [],
+    }
+
+
+# INCISO
+def make_inciso(
+    numero: str = "",
+    texto: str = "",
+    status: str = None,
+) -> Dict[str, Any]:
+    """
+    Schema:
+    {
+        "id_dispositivo_legislativo": "...",
+        "numero_inciso": "...",
+        "texto_do_inciso": "...",
+        "status_do_inciso": "VIGENTE | REVOGADO | SUSPENSO | REDACAO_ANTIGA",
+        "refereiais_legislativos": [...],
+        "modificacoes_historicas": [...],
+        "alineas": [...]
+    }
+    """
+    return{
+        "id_dispositivo_legislativo": gerar_id(),
+        "numero_inciso": numero,
+        "texto_inciso": texto,
+        "status_do_inciso": status,
+        "referenciais_legislativos": [],
+        "modificacoes_historicas": [],
+        "alineas": [],
+    }
+
+
+# ALINEA
+def make_alinea(
+    numero: str = "",
+    texto: str = "",
+    status: str = None,
+) -> Dict[str, Any]:
+    """
+    Schema:
+    {
+        "id_dispositivo_legislativo": "...",
+        "numero_alinea": "...",
+        "texto_da_alinea": "...",
+        "status_da_alinea": "VIGENTE | REVOGADO | SUSPENSO | REDACAO_ANTIGA",
+        "refereiais_legislativos": [...],
+        "modificacoes_historicas": [...]
+    }
+    """
+    return{
+        "id_dispositivo_legislativo": gerar_id(),
+        "numero_alinea": numero,
+        "texto_da_alinea": texto,
+        "status_da_alinea": status,
+        "referenciais_legislativos": [],
+        "modificacoes_historicas": [],
+    }
+
+
+# DETECÇÃO DE ELEMENTOS
 
 
 
-
-
+# CRIAR DRIVER
 def criar_driver (headless: bool = False) -> webdriver.Crhome:
     options = Options()
     if headless:
@@ -67,6 +274,7 @@ def criar_driver (headless: bool = False) -> webdriver.Crhome:
     return driver
 
 
+# CARREGAR PÁGINA
 def carregar_pagina(driver: webdriver.Chrome, url: str) -> str:
     print(f"Acessando: {url}")
     driver.get(url)
@@ -79,12 +287,14 @@ def carregar_pagina(driver: webdriver.Chrome, url: str) -> str:
     return driver.page_source
 
 
-def salvar_json(dados: dict, caminho: Path) -> none:
+# SALVAR DADOS EM JSON
+def salvar_json(dados: dict, caminho: Path) -> None:
     with open(caminho, "w", encoding="utf-8") as f:
         json.dump(dados, f, ensure_ascii=False, indent=2)
     print(f"JSON salvo em: {caminho}")
 
-    
+
+# MAIN
 def main():
     driver = criar_driver(headless=True)
     try:
